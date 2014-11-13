2c812d58349f42c231e3da4eacfadd10aa21a678
Pablo Dorado <pandres95@boolinc.co>
2014-11-13 01:18:31 -0500

    Added string-formatter

--- app/js/auth/services/auth.js
+++ app/js/auth/services/auth.js
@@ -1,8 +1,25 @@
 'use strict';
 
-angular.module('evaluon.auth')
-.factory('Auth', function(){
-  return {
+angular.module('evaluon.auth').factory('Auth', function(api, access, $http){
 
-  };
+    return {
+
+        clientCredentials: function(){
+
+            return $http({
+                method: 'post',
+                url: api.token,
+                data: access.client
+            }).then(function(data){
+                console.log(data);
+            }).catch(function(error){
+                console.log(error);
+            });
+
+        }
+
+
+    };
+
+
 });
