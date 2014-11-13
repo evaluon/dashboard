638f7e9b6d82ea90bdf07a3aa41d73eba339e03a
Pablo Dorado <pandres95@boolinc.co>
2014-11-13 04:33:50 -0500

    Added AuthCtrl for checking loggin and permissions

---
+++ app/js/auth/controllers/auth.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('evaluon.auth').controller('AuthCtrl', function($state){
+
+    // TODO: Add the logic to check if an user is logged in
+    $state.go('anon.login');
+
+});
