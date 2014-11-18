f45179e156209f9127704851420215fb094283f3
Sebastian Osorio <svzosorio@boolinc.co>
2014-11-18 09:18:38 -0500

    Evaluees view

--- app/js/modules/evaluator/controllers/evaluator.js
+++ app/js/modules/evaluator/controllers/evaluator.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('evaluon.evaluator').controller(
-    'EvaluatorCtrl', function($scope, Group){
+    'EvaluatorCtrl', function($scope, $mdDialog, Group){
 
         $scope.groups = [];
 
@@ -15,5 +15,15 @@ angular.module('evaluon.evaluator').controller(
 
         $scope.getGroups();
 
+        $scope.evaluees= function($event, groupId){
+
+            $mdDialog.show({
+                targetEvent: $event,
+                templateUrl: 'views/evaluator/evaluees.tpl.html',
+                controller: 'EvalueesCtrl',
+                onComplete: $scope.getGroups(),
+                locals: { groupId: groupId }
+            });
+        };
     }
 );
