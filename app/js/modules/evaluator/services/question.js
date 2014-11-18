3ceab2fd838d3f344805e4ea4ed3fce52cb5cbee
Pablo Dorado <pandres95@boolinc.co>
2014-11-18 12:41:27 -0500

    [Question] List Knowledge Areas

--- app/js/modules/evaluator/services/question.js
+++ app/js/modules/evaluator/services/question.js
@@ -7,5 +7,23 @@ angular.module('evaluon.evaluator').factory(
             tokenType = user.token_type,
             token = user.access_token;
 
+        return {
+
+            listKnowledgeAreas: function(){
+
+                return $http({
+                    method: 'get',
+                    url: api.knowledgeArea,
+                    headers: {
+                        Authorization: headers.authorization(tokenType, token)
+                    }
+                }).then(function(data){
+                    return data.data.data;
+                })
+
+            }
+
+        };
+
     }
 );
