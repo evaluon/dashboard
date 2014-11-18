2714a9464f7d43a4d7c86e07a5b83a52375d99a2
Sebastian Osorio <svzosorio@boolinc.co>
2014-11-18 08:31:09 -0500

    Finish Statistics

--- app/js/modules/entity/controllers/statistics.js
+++ app/js/modules/entity/controllers/statistics.js
@@ -4,63 +4,85 @@ angular.module('evaluon.entity').controller(
     'StatisticsCtrl', function($scope, Statistics, $timeout){
 
         $scope.statistics = [];
+        $scope.evalueeDescription = {};
 
-        $scope.getStatistics = function(){
+        $scope.getStatisticsData = function(){
+            Statistics.evalueeDescription().then(function(success){
+                $scope.evalueeDescription = success;
+                $scope.setDisabilityData();
+                $scope.setTypeData();
+            });
             Statistics.evalueeList().then(function(success){
                 $scope.statistics = success;
                 $scope.setUsersLevelsData();
+                $scope.setUsersLevelsDataByGender();
+                $scope.setDisabilityData();
+                $scope.setTypeData();
             });
         };
 
-        $scope.getStatistics();
+        $scope.getStatisticsData();
 
-        function sampleData() {
-          var better = 20;
-          return [
-            {
-              key: "hola",
-              val: 100 - better
-            }, {
-              key: "Same",
-              val: better / 4
-            }, {
-              key: "Better",
-              val: better * 3 / 4
-            },
-          ];
-        }
+        //Pies
+        $scope.disabilityData = [{key: '', value: 100}];
+        $scope.typeData = [{key: '', value: 100}];
 
-        $scope.getData = function(){
-            $scope.data = sampleData();
-        }
+        $scope.setDisabilityData = function(){
+            var total = $scope.statistics.length;
+            var data = [];
 
-        $scope.getDisabilityData = function(){
-            return [
-                {
-                    key: "hola"
-                }
-            ]
+            _.each($scope.evalueeDescription.disabilities, function(obj){
+                var val = _.where($scope.statistics, {disability_id : obj.id}).length;
+                data.push({key: obj.description, val: val});
+            });
+
+            $scope.disabilityData = data;
         };
 
-        $scope.data = sampleData();
-        $scope.fixedData = sampleData();
+        $scope.setTypeData = function(){
+            var total = $scope.statistics.length;
+            var data = [];
 
-        //Line
+            _.each($scope.evalueeDescription.types, function(obj){
+                var val = _.where($scope.statistics, {type_id : obj.id}).length;
+                data.push({key: obj.description, val: val});
+            });
+
+            $scope.typeData = data;
+        };
+
+        //Bars
+        $scope.usersLevelsData = [];
+        $scope.usersLevelsDataByGender = [];
+
         $scope.setUsersLevelsData = function(){
+            var value =  _.map($scope.statistics, function(user){ return {label: user.first_name, value: user.average }});
             $scope.usersLevelsData= [
               {
-                'key': 'Nivel general de usuarios',
+                'key': 'Usuario',
                 'color': '#4f99b4',
-                'values' : _.map($scope.statistics, function(user){ return {label: user.first_name, value: user.average  }})
+                'values' : _.sortBy(value, function(o){ return o.value; }).reverse()
               }
             ];
-
-            console.log($scope.usersLevelsData);
         };
 
-        $scope.usersLevelsData = [];
-
-
-        //Bar
-
+        $scope.setUsersLevelsDataByGender = function(){
+            var woman = _.where($scope.statistics, {gender_id : 2});
+            var womanValue =  _.map(woman, function(user){ return {label: user.first_name, value: user.average }});
+            var man = _.where($scope.statistics, {gender_id : 1});
+            var manValue =  _.map(man, function(user){ return {label: user.first_name, value: user.average }});
+            $scope.usersLevelsDataByGender= [
+                {
+                'key': 'Hombres',
+                'color': '#4DB6AC',
+                'values' : _.sortBy(manValue, function(o){ return o.value; }).reverse()
+                },
+                {
+                'key': 'Mujeres',
+                'color': '#F06292',
+                'values' : _.sortBy(womanValue, function(o){ return o.value; }).reverse()
+                }
+            ];
+        };
+        
     });
