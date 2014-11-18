a25df196cfd7a6efc6b656a9b595ce2c89a07185
Sebastian Osorio <svzosorio@boolinc.co>
2014-11-18 12:25:21 -0500

    periodes

--- app/js/modules/evaluator/controllers/evaluator.js
+++ app/js/modules/evaluator/controllers/evaluator.js
@@ -16,11 +16,20 @@ angular.module('evaluon.evaluator').controller(
         $scope.getGroups();
 
         $scope.evaluees= function($event, groupId){
-
             $mdDialog.show({
                 targetEvent: $event,
                 templateUrl: 'views/evaluator/evaluees.tpl.html',
                 controller: 'EvalueesCtrl',
+                onComplete: $scope.getGroups(),
+                locals: { groupId: groupId }
+            });
+        };
+
+        $scope.periode= function($event, groupId){
+            $mdDialog.show({
+                targetEvent: $event,
+                templateUrl: 'views/evaluator/periode.tpl.html',
+                controller: 'PeriodeCtrl',
                 onComplete: $scope.getGroups(),
                 locals: { groupId: groupId }
             });
