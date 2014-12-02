20cd2ebb546bf002612914134f41f326774d88bf
Pablo Dorado <pandres95@boolinc.co>
2014-12-02 10:58:22 -0500

    Typo

--- app/js/modules/institution/controller/addPeriod.js
+++ app/js/modules/institution/controller/addPeriod.js
@@ -2,8 +2,10 @@ angular.module('evaluon.institution').controller(
     'InstitutionAddPeriodCtrl', function($scope, $mdDialog, Period, institution){
 
         $scope.add = function($event, data){
-            Period.createPeriod(institution).then(function(success){
+            Period.createPeriod(institution, data).then(function(success){
                 console.log(success);
             });
         };
-    });
+
+    }
+);
