2fb5f5723a09d00e83cf9f63aa05d213762033f6
Sebastian Osorio <svzosorio@boolinc.co>
2014-12-02 05:29:23 -0500

    period error

--- app/js/modules/institution/controller/updatePeriod.js
+++ app/js/modules/institution/controller/updatePeriod.js
@@ -1,8 +1,8 @@
 angular.module('evaluon.institution').controller(
     'InstitutionUpdatePeriodCtrl', function($scope, $mdDialog, Period, institution, item){
 
-        $scope.data = item;
-        console.log(item)
+        $scope.data = {};
+
         $scope.update = function($event, data){
             Period.updatePeriod(institution).then(function(success){
                 console.log(success);
