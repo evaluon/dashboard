73bf18920c0a36d41b8a29ad8880f95c193d4477
Pablo Dorado <pandres95@boolinc.co>
2014-12-04 11:44:14 -0500

    Approve and deny institution

--- app/js/modules/entity/controllers/approveInstitutions.js
+++ app/js/modules/entity/controllers/approveInstitutions.js
@@ -1,17 +1,43 @@
 'use strict';
 
 angular.module('evaluon.entity').controller(
-    'ApproveInstitutionsCtrl', function($scope, Institution){
+    'ApproveInstitutionsCtrl', function($scope, Institution, toast){
 
         $scope.institutions = [];
 
         $scope.getInstitutions = function(){
+
             Institution.unapprovedInstitutions().then(function(success){
-                console.log(success);
                 $scope.institutions = success;
             });
+
+        };
+
+        $scope.approve = function(id){
+
+            Institution.approveInstitution(id).then(function(){
+                return $scope.getInstitutions();
+            }).then(function(){
+                toast.show("La institución ha sido aprobada correctamente");
+            }).catch(function(response){
+                toast.show(response.error);
+            });
+
+        };
+
+        $scope.reject = function(id, reason){
+
+            Institution.denyInstitution(id, reason).then(function(){
+                return $scope.getInstitutions();
+            }).then(function(){
+                toast.show("La institución ha sido denegada correctamente");
+            }).catch(function(response){
+                toast.show(response.error);
+            });
+
         };
 
         $scope.getInstitutions();
 
-    });
+    }
+);
