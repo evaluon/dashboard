44ff66b9251feb0bf5a0b44602fea6bfd8a8c524
Pablo Dorado <pandres95@boolinc.co>
2014-12-12 02:54:29 -0500

    Fix

--- app/js/modules/institution/controller/addEvaluator.js
+++ app/js/modules/institution/controller/addEvaluator.js
@@ -3,21 +3,6 @@
 angular.module('evaluon.institution').controller('AddEvaluatorCtrl',
 function(Institution, User, Auth, Evaluator, Group, $scope, $mdDialog){
 
-    $scope.user = {
-        id: '',
-        first_name: '',
-        last_name: '',
-
-        mail: '',
-        phone_number: '',
-
-        password: ''
-    };
-
-    $scope.evaluator = {
-        area: ''
-    };
-
     Institution.activeInstitution().then(function(institution){
         $scope.group = {
             evaluator_id: '',
@@ -31,16 +16,9 @@ function(Institution, User, Auth, Evaluator, Group, $scope, $mdDialog){
         Group.addGroup($scope.group).then(function(){
             $mdDialog.hide({ success: true });
         }).catch(function(error){
-            console.error(error);
             $mdDialog.hide(error);
         })
 
     }
-
-    $scope.registerNew = function($event){
-        $event.preventDefault();
-
-        $mdDialog.hide({ success: false });
-    };
 
 });
