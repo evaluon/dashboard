dce1b19397b1cba7bb07a67a5640dda5f92cd99d
Pablo Dorado <pandres95@boolinc.co>
2014-12-12 18:25:34 -0500

    Typo

--- app/js/modules/evaluator/services/test.js
+++ app/js/modules/evaluator/services/test.js
@@ -135,6 +135,7 @@ angular.module('evaluon.evaluator').factory(
                     token = token.access_token;
 
                 return $http({
+                    
                     method: 'post',
                     url: api.testFeedback(test),
                     headers: {
