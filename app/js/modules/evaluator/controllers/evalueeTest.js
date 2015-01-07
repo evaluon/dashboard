d85f4b12ec1a47f6adc41326ce96a52bed20f254
Pablo Dorado <pandres95@boolinc.co>
2015-01-07 08:31:18 -0500

    Json to spanish

--- app/js/modules/evaluator/controllers/evalueeTest.js
+++ app/js/modules/evaluator/controllers/evalueeTest.js
@@ -10,6 +10,7 @@ angular.module('evaluon.evaluator').controller(
         $scope.inEvaluator= $state.includes('evaluator');
 
         $scope.evaluees = [];
+        $scope.evaluees_es = [];
         $scope.headers = [];
 
 
@@ -18,6 +19,24 @@ angular.module('evaluon.evaluator').controller(
                 $scope.groupId, $scope.testId
             ).then(function(success){
                 $scope.evaluees = success;
+                $scope.evaluees_es = _.map(success, function(evaluee){
+                    return {
+                        id: evaluee.id,
+                        "Primer nombre": evaluee.first_name,
+                        "Segundo nombre": evaluee.middle_name,
+                        "Apellidos": evaluee.last_name,
+                        "Fecha de nacimiento": evaluee.birth_date,
+                        "Correo Electrónico": evaluee.mail,
+                        "Número Telefónico": evaluee.phone_number,
+                        "Fecha de Registro": evaluee.register_date,
+                        "Tipo de discapacidad": evaluee.disability,
+                        "Tipo de usuario": evaluee.type,
+                        "Nivel de estudios": evaluee.level,
+                        "Sexo": (evaluee.gender == "male" ? "Hombre" : "Mujer"),
+                        "Nota": evaluee.average,
+                        "Calificado": (evaluee.checked ? "Si" : "No")
+                    }
+                });
                 if($scope.evaluees.length > 0) $scope.headers = Object.keys($scope.evaluees[0]);
             }).catch(function(error){
                 console.error(error);
