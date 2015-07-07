03523b5f262d4518e6d747bd106eb099a5caddb0
Pablo Andrés Dorado Suárez <pandres95@boolinc.co>
2015-07-07 14:57:38 -0500

    Apply filter for image containers

--- app/js/config/api.js
+++ app/js/config/api.js
@@ -92,3 +92,17 @@ angular.module('evaluon').constant(
         evaluon: function(path) { return apiRouter.images('evaluon', path); }
     }
 );
+
+(function(){
+
+    angular
+        .module('evaluon')
+        .value('imgUrlBase', 'http://186.154.240.187:1045')
+        .filter('image', image);
+
+    function image(imgUrlBase) {
+        return function(img){
+            return '{0}/{1}'.format(imgUrlBase, img);
+        };
+    }
+})();
